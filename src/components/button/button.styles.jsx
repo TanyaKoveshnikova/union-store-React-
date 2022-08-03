62b7b098f939b650956b96e5f45a6f59a65cae95
tania <taniakovesh@gmail.com>
2022-08-03 10:47:31 +0500

    try deploy

--- src/components/button/button.styles.jsx
+++ src/components/button/button.styles.jsx
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 
+import { SpinnerContainer } from "../spinner/spinner.styles";
+
 export const BaseButton = styled.button`
   min-width: 10.3rem;
   width: auto;
@@ -18,6 +20,7 @@ export const BaseButton = styled.button`
   display: flex;
   justify-content: center;
   margin: auto;
+  align-items: center;
 
   &:hover {
     background-color: rgb(226, 226, 226);
@@ -48,4 +51,9 @@ export const InvertedButton = styled(BaseButton)`
       border: none;
     }
   }
+`;
+
+export const ButtonSpinner = styled(SpinnerContainer)`
+  width: 1.8rem;
+  height: 1.8rem;
 `;
---
+++ src/components/payment-form/payment-form.component.jsx
@@ -0,0 +1,15 @@
+import "./payment-form.styles";
+// import Button, { BUTTON_TYPES_CLASSES } from "../button/button.component";
+
+const PaymentForm = () => {
+  return (
+    <div>
+      {/* <Button buttonType={BUTTON_TYPES_CLASSES.inverted}>Оплатить</Button> */}
+    </div>
+  );
+};
+export default PaymentForm;
+
+
+//TODO: сделать формочку для заполнения карты 
+
